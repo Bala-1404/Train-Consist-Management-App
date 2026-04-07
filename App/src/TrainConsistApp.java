18ccbc103e1f4aeb4d51bfdb1a40593441945e69
Bala-1404 <baladivyatej14@gmail.com>
2026-04-07 15:18:20 +0530

    PreserveInsertionOrderofBogies

--- App/src/TrainConsistApp.java
+++ App/src/TrainConsistApp.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 import java.util.LinkedList;
 
-
+import java.util.LinkedHashSet;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -11,26 +11,20 @@ import java.util.Set;
 public class TrainConsistApp {
     public static void main(String[] args) {
 
-                System.out.println("===================================");
-                System.out.println("UC4 - Maintain Ordered Bogie Consist");
-                System.out.println("===================================\n");
 
-                LinkedList<String> trainConsist = new LinkedList<>();
+                System.out.println("====================================");
+                System.out.println("UC5 - Preserve Insertion Order of Bogies");
+                System.out.println("====================================\n");
 
-                trainConsist.add("Engine");
-                trainConsist.add("Sleeper");
-                trainConsist.add("AC");
-                trainConsist.add("Cargo");
-                trainConsist.add("Guard");
+                Set<String> formation = new LinkedHashSet<>();
 
-                trainConsist.add(2, "Pantry");
+                formation.add("Engine");
+                formation.add("Sleeper");
+                formation.add("Cargo");
+                formation.add("Guard");
+                formation.add("Sleeper");
 
-                trainConsist.removeFirst();
-                trainConsist.removeLast();
-
-                System.out.println("Final Train Consist:");
-                for (String bogie : trainConsist) {
-                    System.out.println(bogie);
-                }
+                System.out.println("Final Train Formation:");
+                System.out.println(formation);
             }
         }
